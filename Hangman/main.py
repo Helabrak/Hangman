591c0a2eeb3315a01e21ec2f598667983868e540
Heba <84380899+Helabrak@users.noreply.github.com>
2021-06-25 15:56:17 +0200

    Update main.py

--- Hangman/main.py
+++ Hangman/main.py
@@ -12,8 +12,9 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    hangman = Hangman(possible_words = [])
+    hangman.start_game()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
